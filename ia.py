0d4a95c9e99902d6a81e5c9c74acfd5f8485b7c5
JulianMj08 <julianmjadi@gmail.com>
2026-07-05 23:22:47 +0200

    creamos el archivo prompts.py para organizar mejor el codigo, asi separaremos los prompts que utilice la app en un solo archivo

--- ia.py
+++ ia.py
@@ -1,5 +1,7 @@
 from ollama import chat
 
+from prompts import PROMPT_EXTRAER_FACTURA
+
 def extraer_factura(ruta_imagen: str):
 
     response = chat(
@@ -7,20 +9,7 @@ def extraer_factura(ruta_imagen: str):
         messages=[
             {
                 "role": "user",
-                "content": """
-Extrae esta factura y responde únicamente en JSON con:
-
-- numero_factura
-- fecha
-- empresa
-- nif
-- nombre del producto con su precio
-- subtotal
-- iva
-- total
-
-Responde solo con el JSON.
-                """,
+                "content": PROMPT_EXTRAER_FACTURA,
                 "images": [ruta_imagen]
             }
         ]
---
+++ prompts.py
@@ -0,0 +1,14 @@
+PROMPT_EXTRAER_FACTURA = """
+Extrae esta factura y responde únicamente en JSON con:
+
+- numero_factura
+- fecha
+- empresa
+- nif
+- nombre del producto con su precio
+- subtotal
+- iva
+- total
+
+Responde solo con el JSON.
+"""
