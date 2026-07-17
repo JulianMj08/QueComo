46f03e6d10659725f88b1c2fe8581cf45ed62fb1
JulianMj08 <julianmjadi@gmail.com>
2026-07-17 12:24:45 +0200

    Agregamos un cambio en el prompt para que las facturas siempre sse sigan guardando con el nombre exacto del campo que queremos mostrar

--- prompts.py
+++ prompts.py
@@ -5,7 +5,7 @@ Extrae esta factura y responde únicamente en JSON con:
 - fecha
 - empresa
 - nif
-- productos
+- productos (CADA PRODUCTO DEBE DE IR EJEMPLO {"nombre": "FIDEO ALIPENDE 1KG CABELLIN", "precio": "1,20\u20ac"} NO "descripcion": "LECHE ALIPENDE 1L ENTERA" NI NINGUN OTRO SINOMIMO)
 - subtotal
 - iva
 - total
